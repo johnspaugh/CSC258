2f6c00f233084546dc72c4f00e46eb558e02e87e
John Spaugh <johnspaugh33@gmail.com>
2026-03-26 14:01:30 -0700

    notes from the meeting to include and improve

--- JSON_docker/recievedData.py
+++ JSON_docker/recievedData.py
@@ -18,3 +18,14 @@ info = json.loads(data)
 print('Name:', info["username"])
 print('Hide:', info["email"]["hide"])
 
+# good things to pull when examining MichealSampleData file
+# display_name
+# text
+# created_at
+# handle
+# reply (Maybe if its not wierd) ref
+# tags (if not have then don't include)
+# indexed_at
+# python map
+# 25 posts #
+
