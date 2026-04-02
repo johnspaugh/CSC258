ed938efd4cd682b66088624e3f26450956491056
Taro Kumagai <tckumaga@gmail.com>
2026-04-02 02:55:41 -0700

    Git ignore + muscle bot

---
+++ Bot_docker/MuscleBot/Program.cs
@@ -0,0 +1,60 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using MuscleBot.commands;
+using MuscleBot.config;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuscleBotTest
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            JSONReader reader = new JSONReader();
+
+            await reader.ReadJSON();
+
+            DiscordConfiguration discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = reader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { reader.prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false,
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<TestCommands>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+}
