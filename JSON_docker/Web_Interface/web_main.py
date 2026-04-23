d37642cd90dff407e238817b01d16745e07e612a
John Spaugh <johnspaugh33@gmail.com>
2026-04-23 11:04:39 -0700

    FASTAPI intial setup, and connect to receiveList.py, but need to connect to dataIngestion

--- JSON_docker/Web_Interface/web_main.py
+++ JSON_docker/Web_Interface/web_main.py
@@ -8,6 +8,14 @@
 # python -m pip install fastapi uvicorn
 # python -m venv venv
 # pip show uvicorn
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT / "docker-project258Updated" / "dataIngestion" / "bluesky"))
+from bsky import get_posts
+
+
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -28,4 +36,16 @@ def read_index():
 def hello(name: str = "World"):
     return {"message": f"Hello, {name}!"}
 
+# call on dataIngestion to get the processed data in JSON format
 
+
+# once dataProcessing and dataIngestion are done, 
+# we can call the get_processed_data_json function to retrieve the processed data in JSON format and return it through an API endpoint
+from receivedData import get_processed_data_json
+@app.get("/api/processed-data")
+def get_processed_data():
+    json_data = get_processed_data_json()
+    if json_data is not None:
+        return {"processed_data": json_data}
+    else:
+        return {"error": "Processed data not available"}
